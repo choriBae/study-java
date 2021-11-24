85664ca25371255718eb7c9836764f2981691d22
chori <chori.dev@gmail.com>
2021-11-24 18:53:57 +0900

    반복문 학습 for 기본문, 향상된 for문 while, do-while문 continue문

--- study-java/src/study/SquidGame.java
+++ study-java/src/study/SquidGame.java
@@ -12,8 +12,9 @@ public class SquidGame {
 		*/
 		
 		String title = "SquidGame";
-		int people = 456;
-		int myMoney = 1;
+		int people = 456,
+			myMoney = 1;
+		String[] array = {"고구마","감자","치팅","계란","닭가슴살"}; 
 		
 		//조건문
 		if (people > 10) {
@@ -45,6 +46,41 @@ public class SquidGame {
 		}
 		
 		//반복문
+		for(int i=0; i < array.length; i++) {
+			System.out.println("일반 for문 = " + array[i]);
+		}
+		
+		for(String stmp : array) {
+			System.out.println("향상된 for문 = "+ stmp);
+		}
+		
+		
+		int count = 5;
+		while (count != 5) {
+			System.out.println(count);
+			count++;
+		}
+		
+		int count2 = 4;
+		do {
+			//System.out.println(count2);
+			count2++;
+			
+			if(count2 == 6)
+			break;
+		} while (count != 6);
+		
+		for(String stmp : array) {
+			System.out.println("오늘의 식단은 "+ stmp);
+			
+			if(stmp == "치팅") {
+				System.out.println("치킨이다!!");
+				continue;
+			}
+			
+			System.out.println("웩");
+		}
+			
 	}
 
 }
